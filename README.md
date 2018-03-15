8f70b600a6b1d35bc0861ad777e13d75bfa92452
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-03-14 17:01:21 -0700

    Chapter 15: Introduction to Node.js

---
+++ README.md
@@ -0,0 +1,38 @@
+# Chattrbox
+
+Sets up Node.js for Chattrbox's server. Backend that allows users to chat with another user. This is for **Homework 5 - CPSC 473 Front-End Development course**.
+
+## Getting Started
+
+**Clone** or **download** the repo
+
+### Install
+
+[`Node.js`](https://nodejs.org/en/) and follow the prompts.
+
+`npm init` on terminal to initialize package.json
+
+`npm install --save-dev nodemon` on terminal
+
+
+### Run In Terminal
+
+`cd/directory-of-Chattrbox/Chattrbox-master` change _directory-of-Chattrbox_ to the directory in which you downloaded the repo
+
+`npm start`
+
+`npm run dev` if you need to restart the service
+
+### Several Explanations
+
+**In Package.json**
+
+| Commands | Meaning |
+| :---: | :---: |
+| npm install --save-dev nodemon  | We don't have to keep quitting/rerunning on terminal  |
+| npm start (in package.json)     | Starts Node server  |
+| nodemon module (in package.json)| Automates the work of restarting the service (don't have to stop in terminal then do the npm start command again) |
+
+## Demo
+
+<!-- ![alt text](Coffeerun2-Demo.gif) -->
---
+++ index.js
@@ -0,0 +1,36 @@
+var http = require("http");
+var fs = require("fs"); //import Node.js file system module
+//var path = require("path"); //finds file that was requested
+var extract = require("./extract"); //uses extract.js
+
+var handleError = function(err, res) {
+  res.writeHead(404);
+  res.end();
+};
+
+//function is called for every HTTP request
+var server = http.createServer(function(req,res){
+  console.log("Responding to a request.");
+  //res.end("<h1>Hello, World!!</h1>");
+  //callback checks what file the browser is requesting
+  //var url = req.url;
+  //var fileName = "index.html";
+  //if(url.length > 1) {
+  //  fileName = url.substring(1); //if not index.html, it gets rid of the first character '/'
+  //}
+  //console.log(fileName);
+  //var filePath = path.resolve(__dirname, "app", fileName);
+  //readFile method takes in a filename and a callback
+  //inside the callback, the contents of file are sent
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function(err, data){
+    if(err) {
+      handleError(err, res);
+      return;
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000); //binds to a port
